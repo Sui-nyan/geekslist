e91bd142cddc7b35f2fe2c27d5ed694229533a58
peanutted <78491387+peanutted@users.noreply.github.com>
2023-04-10 15:46:33 +0200

    Initial commit

---
+++ src/main/java/hdm/stuttgart/geekslist/GeekslistApplication.java
@@ -0,0 +1,13 @@
+package hdm.stuttgart.geekslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hdm/stuttgart/geekslist/GeekslistApplicationTests.java
@@ -0,0 +1,13 @@
+package hdm.stuttgart.geekslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
